5aeef5f81d2b454a5c4bc25eb6608901329e60f5
Eduardo Fernandez <fernandez9000@gmail.com>
2018-09-28 14:12:42 -0500

    blog app, first commit

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.text import slugify
+# Create your models here.
+
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(default='',blank=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def save(self):
+    	self.slug = slugify(self.title)
+    	super(Post,self).save()
+
+    def __str__(self):
+        return self.title
---
+++ stocksportfolio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StocksportfolioConfig(AppConfig):
+    name = 'stocksportfolio'
