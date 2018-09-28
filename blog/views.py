ebecae1fd7839ed13aa215c1c84a8ecfd127416e
eduFDiaz <fernandez9000@gmail.com>
2018-09-28 15:34:41 -0500

    post_list template and views created

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
--- codingindfw/urls.py
+++ codingindfw/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+	## leave below line when not want to redirect index to blog urls
+	#path('blog/', include('blog.urls')), 
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
