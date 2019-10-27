9cb0ac7ed33d770a4567a65ba7c003851b5cfc39
Eduardo A Fernandez Diaz <fernandez9000@gmail.com>
2019-10-27 12:27:01 -0500

    clean up done

--- blog/api/urls.py
+++
@@ -1,8 +0,0 @@
-from .views import CategoryViewSet, PostViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet, basename='category')
-router.register(r'posts', PostViewSet, basename='post')
-urlpatterns = router.urls
-print(urlpatterns)
--- configuration.sh
+++
@@ -1,6 +0,0 @@
-# Create a system user named coding assign it to a system group called webapps
-sudo groupadd --system webapps
-sudo useradd --system --gid webapps --home /home/pi/webapps/codingindfw-env coding
-
-# Now change the owner of files in each application’s folder. I like to assign the group users as the owner, because that allows regular users of the server to access and modify parts of the application which are group-writable. This is optional.
-sudo chown -R coding:users /home/pi/webapps/codingindfw-env
