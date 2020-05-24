fb0419a97d656ec214436d332f705781372c2713
dcruz1990 <dcruzbv1990@gmail.com>
2020-05-24 09:43:49 -0400

    [Frontend] Faq service added

---
+++ coding-frontend/src/app/_models/Faq.ts
@@ -0,0 +1,6 @@
+export interface Faq {
+    userId?: string,
+    title: string,
+    description: string
+    id: string
+}
---
+++ coding-frontend/src/app/_services/faq-service.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Faq } from '../_models/Faq';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FaqServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  getFaqs(userid: string): Observable<Faq> {
+    return this.http.get<Faq>(environment.apiUrl + '/faq/all')
+  }
+
+  newFaq(data: Faq) {
+    return this.http.post(environment.apiUrl + '/faq/create', data)
+  }
+
+  editFaq(id: string, data: Faq) {
+    return this.http.put(environment.apiUrl + '/faq/' + id + '/update', data)
+  }
+
+  deleteFaq(id: string) {
+    return this.http.delete(environment.apiUrl + '/faq/' + id + '/delete')
+  }
+
+}
