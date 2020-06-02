52dd9c4728d6c3876d63cb1571649274435211c2
dcruz1990 <dcruzbv1990@gmail.com>
2020-06-02 14:51:34 -0400

    [Frontend] Portfolio ok

--- coding-frontend/src/app/_pipes/sort-by.pipe.ts
+++ coding-frontend/src/app/_pipes/sort-by.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform, Injectable } from '@angular/core';
-
 @Pipe({
   name: 'sortby'
 })
 
 @Injectable()
 export class SortByPipe implements PipeTransform {
+
   transform(array: Array<any>, filterField: string, filterValue: any): Array<any> {
-    if (!array) return [];
+    if (!array) return []
     if (filterValue == 'all') {
       return array
     } else
